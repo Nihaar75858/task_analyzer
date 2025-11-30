49129b63a9b0c65bbf677fda996515bc0dbfd321
Nihaar75858 <nihar.adhiep@gmail.com>
2025-11-30 14:05:35 +0530

    Implemented validators within Task model for estimated_hrs and importance

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=255)
     due_date = models.DateField()
-    estimated_hrs = models.FloatField()
-    importance = models.IntegerField()
+    estimated_hrs = models.FloatField(
+        validators=[MinValueValidator(0.5)]
+    )
+    importance = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
     dependencies = models.JSONField(default=list, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
